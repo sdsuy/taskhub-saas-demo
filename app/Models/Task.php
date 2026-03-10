0c0010324d5e18ad157d40bfc4d3c59874100b10
sdsuy <santiagodasilva@sdsuy.dev>
2026-03-10 14:29:34 -0300

    feat: automatically assign tenant_id to tasks

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'tenant_id',
+        'status'
+    ];
+
+    protected static function booted()
+    {
+        statis::creating(function ($task) {
+            $task->tenant_id = currentTenant()->id;
+        });
+    }
 }
